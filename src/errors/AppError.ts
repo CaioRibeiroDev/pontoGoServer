789ce1052c72711536279501dc462db21b47e547
Caio Anderson Ribeiro <69832122+CaioRibeiroDev@users.noreply.github.com>
2022-10-10 16:54:41 -0300

    feat(master): add prisma

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -1,7 +1,6 @@
 export class AppError {
   public readonly message: string
   public readonly statusCode: number
-  public readonly errorCode:number
 
   constructor (message: string, statusCode = 400) {
     this.message = message
